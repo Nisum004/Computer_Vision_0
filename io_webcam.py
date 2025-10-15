7ff40795ea68db8b14c808972bff5ec60ef073dc
Nisum004 <izanamitube@gmail.com>
2025-10-15 22:00:34 +0545

    update

---
+++ io_webcam.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+import webcam
+
+# read webcam
+webcam = cv2.VideoCapture(0) # 1,2 if more than one webcam
+
+# visualize
+while True:
+    ret , frame = webcam.read()
+    cv2.imshow('frame' , frame)
+    if cv2.waitKey(40) & 0xFF == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
